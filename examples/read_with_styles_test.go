a6a2f493996ea8d281aa1b83e547fefa19809de6
IsAurora6 <85173010+IsAurora6@users.noreply.github.com>
2025-08-10 16:40:30 +0800

    feat: fix bug that text content was incorrectly splitted in TJ cases and add test cases (#3)

---
+++ examples/read_with_styles_test.go
@@ -0,0 +1,31 @@
+package examples
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/casibase/pdf"
+)
+
+func TestGetStyledText(t *testing.T) {
+	f, r, err := pdf.Open("./pdf_test.pdf")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	sentences, err := r.GetStyledTexts()
+	if err != nil {
+		panic(err)
+	}
+
+	// Print all sentences
+	for _, sentence := range sentences {
+		fmt.Printf("Font: %s, Font-size: %f, x: %f, y: %f, content: %s \n",
+			sentence.Font,
+			sentence.FontSize,
+			sentence.X,
+			sentence.Y,
+			sentence.S)
+	}
+}
